6578b61a5c67432e733d9c76c57c431f9d088b68
Subhangi Ringasia <59997192+subhangiii@users.noreply.github.com>
2022-07-24 07:18:50 +0530

    Add files via upload

---
+++ Googl/src/contexts/StateContextProvider.js
@@ -0,0 +1,41 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const StateContext = createContext();
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+
+export const StateContextProvider = ({ children }) => {
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const getResults = async (url) => {
+    setLoading(true);
+
+    const res = await fetch(`${baseUrl}${url}`, {
+      method: 'GET',
+      headers: {
+        'X-User-Agent': 'desktop',
+        'X-Proxy-Location': 'EU',
+        'X-RapidAPI-Key': 'a3028b10a2msh3d33fc0c437cfa9p1366b5jsn328921c15565',
+        'X-RapidAPI-Host': 'google-search3.p.rapidapi.com'
+      },
+    });
+
+    const data = await res.json();
+
+    console.log(data);
+
+    setResults(data);
+    setLoading(false);
+  };
+
+  return (
+    <StateContext.Provider
+      value={{ getResults, results, searchTerm, setSearchTerm, loading }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
